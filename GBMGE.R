12eea7183eaef36d54acc58efa5f97c72e02da9f
Min-Sub Kim <36905975+lsoda@users.noreply.github.com>
2021-06-15 16:42:03 +0900

    Update GBMGE.R

--- GBMGE.R
+++ GBMGE.R
@@ -1,23 +1,28 @@
 setwd("D://Lab//OA//Pn_OA//3replicate//2rep_dmg")
 getwd()
 
+# File load GBM, RNASeq
 changeF0 = read.table('./F0_table.txt', header = T)
 degF0 = read.table('./deg_F0.csv', header = T, sep=',', row.names=1)
 
+# Methylation Fraction file
 classes = read.table('./logfrac.txt', header = T)
 
 head(changeF0)
+
+# Polish file
 changeF0$logDMG=as.numeric(as.character(changeF0$logDMG))
 changeF0$logFC=as.numeric(as.character(changeF0$logFC))
 degF0$logFC=as.numeric(as.character(degF0$logFC))
 degF0$FDR=as.numeric(as.character(degF0$FDR))
 
+# Merge tables
 x=merge(classes,changeF0, by="gene")
 row.names(x)<-x$gene
 y=deg
 
 
-#------------------- GBM ~ GE
+# GBM ~ Expression analysis
 goods=intersect(row.names(x),row.names(y))
 length(goods)
 y=y[goods,]
@@ -26,7 +31,7 @@ summary(x)
 siglimit=1
 sigs=(x$padj<=siglimit)
 plot(changeF0$logFC~changeF0$logDMG,col=rgb(0,0,0,alpha=0.1),pch=16,cex=0.5,family= "serif", mgp=c(2.3,1,0),xlim=c(-5,5),ylim=c(-10,10))
-title(xlab="GBM change", ylab = "Gene Expression (logFC)", family = "serif", cex.lab = 1)
+title(xlab="GBM change", ylab = "Gene Exp:ssion (logFC)", family = "serif", cex.lab = 1)
 abline(v=0,col="grey70",lty=3)
 abline(v=0.15,col="grey70",lty=3)
 abline(v=-0.15,col="grey70",lty=3)
